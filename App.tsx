6a3df4e414c7a5f07547dcf7a4228f3ba5991c67
dhonata <dhonatafreitas@gmail.com>
2022-11-20 20:15:43 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import { SingIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/style/theme'
+
+export default function App() {
+  
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      { fontsLoaded ? <SingIn /> : <Loading /> }
+
+    </NativeBaseProvider>
+  );
+}
